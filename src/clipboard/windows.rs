baeb613fe45fef6d252dae4fdce5cfb7054f50e2
Artyom <artyomnebyansky@icloud.com>
2025-02-16 15:18:34 +0100

    draft for win

--- src/clipboard/windows.rs
+++ src/clipboard/windows.rs
@@ -1,6 +1,41 @@
 use super::{Clipboard, ClipboardData, ClipboardError};
+use winapi::shared::ntdef::{FALSE, NULL};
+use winapi::shared::windef::HWND__;
+use winapi::um::winuser::{CloseClipboard, EnumClipboardFormats, OpenClipboard};
 
-struct WindowsClipboard;
+pub struct WindowsClipboard;
+
+impl WindowsClipboard {
+    fn open() -> Result<(), ClipboardError> {
+        unsafe {
+            let clipboard = OpenClipboard(NULL as *mut HWND__);
+
+            if clipboard == FALSE.into() {
+                return Err(ClipboardError::Init("Failed to open clipboard".to_string()));
+            }
+            Ok(())
+        }
+    }
+    fn close() -> Result<(), ClipboardError> {
+        unsafe {
+            let success = CloseClipboard();
+            if success == FALSE.into() {
+                return Err(ClipboardError::Init(
+                    ("Failed to close clipboard".to_string()),
+                ));
+            }
+            Ok(())
+        }
+    }
+
+    fn read_text() -> Result<ClipboardData, ClipboardError> {
+        todo!()
+    }
+
+    fn read_file() -> Result<ClipboardData, ClipboardError> {
+        todo!()
+    }
+}
 
 impl Clipboard for WindowsClipboard {
     fn init() -> Result<Self, ClipboardError> {
@@ -10,46 +45,43 @@ impl Clipboard for WindowsClipboard {
         todo!()
     }
     fn read(&self) -> Result<ClipboardData, ClipboardError> {
-        todo!()
-        // unsafe {
-        //     // Open clipboard (NULL or GetDesktopWindow())
-        //     if OpenClipboard(GetDesktopWindow()).is_err() {
-        //         return Err(ClipboardError::Read("Failed to open clipboard".to_string()));
-        //     }
-        //
-        //     let mut format = 0;
-        //     let mut latest_format = 0;
-        //
-        //     // Find the latest available clipboard format
-        //     while {
-        //         format = EnumClipboardFormats(format);
-        //         format != 0
-        //     } {
-        //         latest_format = format;
-        //     }
-        //
-        //     if latest_format == 0 {
-        //         CloseClipboard();
-        //         return Err(ClipboardError::Read(
-        //             "No clipboard formats found".to_string(),
-        //         ));
-        //     }
-        //
-        //     // Handle different clipboard formats
-        //     let result = match latest_format {
-        //         CF_UNICODETEXT => Self::read_text(),
-        //         CF_HDROP => Self::read_files(),
-        //         CF_DIB => Err(ClipboardError::InitFailed(
-        //             "Clipboard contains an image (DIB)".to_string(),
-        //         )),
-        //         _ => Err(ClipboardError::Read(format!(
-        //             "Unsupported format: {}",
-        //             latest_format
-        //         ))),
-        //     };
-        //
-        //     CloseClipboard();
-        //     result
-        // }
+        unsafe {
+            // Open clipboard (NULL or GetDesktopWindow())
+            WindowsClipboard::open()?;
+
+            let mut format = 0;
+            let mut latest_format = 0;
+
+            // Find the latest available clipboard format
+            while {
+                format = EnumClipboardFormats(format);
+                format != 0
+            } {
+                latest_format = format;
+            }
+
+            if latest_format == 0 {
+                WindowsClipboard::close()?;
+                return Err(ClipboardError::Read(
+                    "No clipboard formats found".to_string(),
+                ));
+            }
+
+            // Handle different clipboard formats
+            let result = match latest_format {
+                CF_UNICODETEXT => Self::read_text(),
+                CF_HDROP => Self::read_file(),
+                CF_DIB => Err(ClipboardError::Read(
+                    "Clipboard contains an image (DIB)".to_string(),
+                )),
+                _ => Err(ClipboardError::Read(format!(
+                    "Unsupported format: {}",
+                    latest_format
+                ))),
+            };
+
+            WindowsClipboard::close()?;
+            result
+        }
     }
 }
