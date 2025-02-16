ab9382ae663b614f7ad05ecf0558f7087ed85031
Artyom nebyansky <artyom.nebyansky@taxmaro.com>
2025-02-16 15:46:39 +0100

    macos clipboard refactors

--- src/clipboard/macos.rs
+++ src/clipboard/macos.rs
@@ -11,11 +11,39 @@ use objc::sel_impl;
 use std::ffi::CStr;
 use std::ffi::CString;
 use std::fs;
+use std::str::FromStr;
 
 #[link(name = "AppKit", kind = "framework")]
 extern "C" {}
 
 type ObjectId = *mut Object;
+
+#[allow(clippy::upper_case_acronyms)]
+enum PasteboardType {
+    FILEPATH,
+    IMAGE,
+    TEXT,
+}
+
+impl FromStr for PasteboardType {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<PasteboardType, Self::Err> {
+        if input == "public.utf8-plain-text" {
+            return Ok(PasteboardType::TEXT);
+        }
+
+        if input == "public.file-url" {
+            return Ok(PasteboardType::FILEPATH);
+        }
+        if input.starts_with("public.")
+            && (input.contains("png") || input.contains("jpeg") || input.contains("tiff"))
+        {
+            return Ok(PasteboardType::IMAGE);
+        }
+        Err(())
+    }
+}
 
 pub struct MacosClipboard {
     p: ObjectId,
@@ -23,12 +51,50 @@ pub struct MacosClipboard {
 
 #[allow(unexpected_cfgs)]
 impl MacosClipboard {
-    fn read_file(&self, path: &str) -> AnyResult<Vec<u8>> {
+    fn open_file(&self, path: &str) -> AnyResult<Vec<u8>> {
         let file = fs::read(path)?;
 
         Ok(file)
     }
-
+    fn read_file(&self, first_type: *mut Object) -> Result<ClipboardData, ClipboardError> {
+        let pb = self.p;
+        unsafe {
+            let ns_url: *mut Object = msg_send![pb, propertyListForType: first_type];
+            if ns_url.is_null() {
+                return Err(ClipboardError::Read(
+                    "Failed to read file URL from clipboard".to_string(),
+                ));
+            }
+            let utf8: *const i8 = msg_send![ns_url, UTF8String];
+            let c_str = CStr::from_ptr(utf8).to_string_lossy();
+            debug_println!("File: {}", c_str);
+            if c_str.ends_with("/") {
+                return Err(ClipboardError::Read(String::from("Cannot copy folders")));
+            }
+            let file_buf = self.open_file(&c_str).map_err(|err| {
+                ClipboardError::Read(format!("Failed to read file from buffer: {:?}", err))
+            })?;
+            let filename = c_str.split("/").last().unwrap_or("unknown_file");
+            Ok(ClipboardData::File((String::from(filename), file_buf)))
+        }
+    }
+    fn read_text(&self, first_type: ObjectId) -> Result<ClipboardData, ClipboardError> {
+        unsafe {
+            let pb = self.p;
+            let ns_string: *mut Object = msg_send![pb, stringForType: first_type];
+            if ns_string.is_null() {
+                return Err(ClipboardError::Read(
+                    "Failed to read text from clipboard".to_string(),
+                ));
+            }
+            let utf8: *const i8 = msg_send![ns_string, UTF8String];
+            let c_str = CStr::from_ptr(utf8);
+            debug_println!("Text: {}", c_str.to_string_lossy());
+            Ok(ClipboardData::String(
+                c_str.to_string_lossy().as_bytes().to_vec(),
+            ))
+        }
+    }
     fn read_image(&self, first_type: *mut Object) -> Result<Vec<u8>, ClipboardError> {
         let pb = self.p;
         unsafe {
@@ -54,6 +120,93 @@ impl MacosClipboard {
             Ok(slice.to_vec())
         }
     }
+
+    fn write_file(&self, file_data: (String, Vec<u8>)) -> Result<(), ClipboardError> {
+        let pb = self.p;
+        unsafe {
+            let (filename, data) = file_data;
+            let mime_type_opt = filename.split(".").last();
+            if mime_type_opt.is_none() {
+                return Err(ClipboardError::Write(
+                    "Failed to write bytes into clipboard. Unknown mime_type".to_string(),
+                ));
+            }
+            let mime_type = mime_type_opt.unwrap();
+            // Convert Rust byte slice to NSData
+            let ns_data: *mut Object =
+                msg_send![class!(NSData), dataWithBytes: data.as_ptr() length: data.len()];
+            if ns_data.is_null() {
+                return Err(ClipboardError::Write("Failed to create NSData".to_string()));
+            }
+
+            // Convert MIME type to NSString
+            let c_type = CString::new(mime_type).unwrap();
+            let ns_type: *mut Object =
+                msg_send![class!(NSString), stringWithUTF8String: c_type.as_ptr()];
+            if ns_type.is_null() {
+                return Err(ClipboardError::Write(
+                    "Failed to create NSString for MIME type".to_string(),
+                ));
+            }
+
+            // Clear clipboard
+            let _: () = msg_send![pb, clearContents];
+
+            // Store binary data into clipboard
+            let success: bool = msg_send![pb, setData: ns_data forType: ns_type];
+            if !success {
+                return Err(ClipboardError::Write(
+                    "Failed to write binary data to clipboard".to_string(),
+                ));
+            }
+
+            debug_println!("Binary data written to clipboard as {}", mime_type);
+            Ok(())
+        }
+    }
+    fn write_text(&self, text: &[u8]) -> Result<(), ClipboardError> {
+        let pb = self.p;
+        unsafe {
+            let text = String::from_utf8(text.to_vec()).map_err(|err| {
+                ClipboardError::Write(format!("Failed to write string into clipboard: {:?}", err))
+            })?;
+            // Convert Rust `&str` to `NSString`
+            let c_text = CString::new(text.as_str()).unwrap();
+            let ns_string: *mut Object =
+                msg_send![class!(NSString), stringWithUTF8String: c_text.as_ptr()];
+
+            if ns_string.is_null() {
+                return Err(ClipboardError::Write(
+                    "Failed to create NSString".to_string(),
+                ));
+            }
+
+            let c_public_text_type = CString::new("public.utf8-plain-text").map_err(|err| {
+                ClipboardError::Write(format!(
+                    "Failed to create public text type C string: {:?}",
+                    err
+                ))
+            })?;
+            // Define the public text type
+            let utf8_type: *mut Object =
+                msg_send![class!(NSString), stringWithUTF8String: c_public_text_type.as_ptr()];
+
+            // Clear clipboard before writing
+            let _: () = msg_send![pb, clearContents];
+
+            // Write the text to the clipboard
+            let success: bool = msg_send![pb, setString: ns_string forType: utf8_type];
+
+            if !success {
+                return Err(ClipboardError::Write(
+                    "Failed to write text to clipboard".to_string(),
+                ));
+            }
+
+            debug_println!("Text written to clipboard: {}", text);
+            Ok(())
+        }
+    }
 }
 
 #[allow(unexpected_cfgs)]
@@ -71,94 +224,9 @@ impl Clipboard for MacosClipboard {
         Ok(MacosClipboard { p: pb })
     }
     fn write(&self, data: ClipboardData) -> Result<(), ClipboardError> {
-        let pb = self.p;
-
         match data {
-            ClipboardData::File(file_data) => {
-                unsafe {
-                    let (filename, data) = file_data;
-                    let mime_type_opt = filename.split(".").last();
-                    if mime_type_opt.is_none() {
-                        return Err(ClipboardError::Write(
-                            "Failed to write bytes into clipboard. Unknown mime_type".to_string(),
-                        ));
-                    }
-                    let mime_type = mime_type_opt.unwrap();
-                    // Convert Rust byte slice to NSData
-                    let ns_data: *mut Object =
-                        msg_send![class!(NSData), dataWithBytes: data.as_ptr() length: data.len()];
-                    if ns_data.is_null() {
-                        return Err(ClipboardError::Write("Failed to create NSData".to_string()));
-                    }
-
-                    // Convert MIME type to NSString
-                    let c_type = CString::new(mime_type).unwrap();
-                    let ns_type: *mut Object =
-                        msg_send![class!(NSString), stringWithUTF8String: c_type.as_ptr()];
-                    if ns_type.is_null() {
-                        return Err(ClipboardError::Write(
-                            "Failed to create NSString for MIME type".to_string(),
-                        ));
-                    }
-
-                    // Clear clipboard
-                    let _: () = msg_send![pb, clearContents];
-
-                    // Store binary data into clipboard
-                    let success: bool = msg_send![pb, setData: ns_data forType: ns_type];
-                    if !success {
-                        return Err(ClipboardError::Write(
-                            "Failed to write binary data to clipboard".to_string(),
-                        ));
-                    }
-
-                    debug_println!("Binary data written to clipboard as {}", mime_type);
-                }
-            }
-            ClipboardData::String(data) => {
-                unsafe {
-                    let text = String::from_utf8(data).map_err(|err| {
-                        ClipboardError::Write(format!(
-                            "Failed to write string into clipboard: {:?}",
-                            err
-                        ))
-                    })?;
-                    // Convert Rust `&str` to `NSString`
-                    let c_text = CString::new(text.as_str()).unwrap();
-                    let ns_string: *mut Object =
-                        msg_send![class!(NSString), stringWithUTF8String: c_text.as_ptr()];
-
-                    if ns_string.is_null() {
-                        return Err(ClipboardError::Write(
-                            "Failed to create NSString".to_string(),
-                        ));
-                    }
-
-                    let c_public_text_type =
-                        CString::new("public.utf8-plain-text").map_err(|err| {
-                            ClipboardError::Write(format!(
-                                "Failed to create public text type C string: {:?}",
-                                err
-                            ))
-                        })?;
-                    // Define the public text type
-                    let utf8_type: *mut Object = msg_send![class!(NSString), stringWithUTF8String: c_public_text_type.as_ptr()];
-
-                    // Clear clipboard before writing
-                    let _: () = msg_send![pb, clearContents];
-
-                    // Write the text to the clipboard
-                    let success: bool = msg_send![pb, setString: ns_string forType: utf8_type];
-
-                    if !success {
-                        return Err(ClipboardError::Write(
-                            "Failed to write text to clipboard".to_string(),
-                        ));
-                    }
-
-                    debug_println!("Text written to clipboard: {}", text);
-                }
-            }
+            ClipboardData::File(file_data) => self.write_file(file_data)?,
+            ClipboardData::String(data) => self.write_text(data.as_slice())?,
         }
         Ok(())
     }
@@ -184,53 +252,20 @@ impl Clipboard for MacosClipboard {
             let type_cstr = CStr::from_ptr(type_utf8);
             let type_str = type_cstr.to_string_lossy().into_owned();
 
-            // Handle different types
-            if type_str == "public.utf8-plain-text" {
-                let ns_string: *mut Object = msg_send![pb, stringForType: first_type];
-                if ns_string.is_null() {
-                    return Err(ClipboardError::Read(
-                        "Failed to read text from clipboard".to_string(),
-                    ));
+            let p_type = PasteboardType::from_str(&type_str).map_err(|err| {
+                ClipboardError::Read(format!("Unknown pasteboard type: {:?}", err))
+            })?;
+
+            match p_type {
+                PasteboardType::TEXT => self.read_text(first_type),
+                PasteboardType::IMAGE => {
+                    debug_println!("Image format detected: {}", type_str);
+                    let mime_type = type_str.split(".").last().unwrap_or("png");
+                    let filename = format!("image.{}", mime_type);
+                    let buff = self.read_image(first_type)?;
+                    Ok(ClipboardData::File((filename, buff)))
                 }
-                let utf8: *const i8 = msg_send![ns_string, UTF8String];
-                let c_str = CStr::from_ptr(utf8);
-                debug_println!("Text: {}", c_str.to_string_lossy());
-                Ok(ClipboardData::String(
-                    c_str.to_string_lossy().as_bytes().to_vec(),
-                ))
-            } else if type_str == "public.file-url" {
-                let ns_url: *mut Object = msg_send![pb, propertyListForType: first_type];
-                if ns_url.is_null() {
-                    return Err(ClipboardError::Read(
-                        "Failed to read file URL from clipboard".to_string(),
-                    ));
-                }
-                let utf8: *const i8 = msg_send![ns_url, UTF8String];
-                let c_str = CStr::from_ptr(utf8).to_string_lossy();
-                debug_println!("File: {}", c_str);
-                if c_str.ends_with("/") {
-                    return Err(ClipboardError::Read(String::from("Cannot copy folders")));
-                }
-                let file_buf = self.read_file(&c_str).map_err(|err| {
-                    ClipboardError::Read(format!("Failed to read file from buffer: {:?}", err))
-                })?;
-                let filename = c_str.split("/").last().unwrap_or("unknown_file");
-                Ok(ClipboardData::File((String::from(filename), file_buf)))
-            } else if type_str.starts_with("public.")
-                && (type_str.contains("png")
-                    || type_str.contains("jpeg")
-                    || type_str.contains("tiff"))
-            {
-                debug_println!("Image format detected: {}", type_str);
-                let mime_type = type_str.split(".").last().unwrap_or("png");
-                let filename = format!("image.{}", mime_type);
-                let buff = self.read_image(first_type)?;
-                Ok(ClipboardData::File((filename, buff)))
-            } else {
-                Err(ClipboardError::Read(format!(
-                    "Clipboard contains unsupported type: {}",
-                    type_str
-                )))
+                PasteboardType::FILEPATH => self.read_file(first_type),
             }
         }
     }
