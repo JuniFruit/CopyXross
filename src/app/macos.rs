bf7847b45b4279b67cddf10451f69460313f4059
Artyom nebyansky <artyom.nebyansky@taxmaro.com>
2025-03-19 18:31:48 +0100

    core taskbar functionality

--- src/app/macos.rs
+++ src/app/macos.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
-use std::ffi::c_void;
 use std::ffi::CStr;
 use std::ffi::CString;
+use std::ptr;
 use std::sync::Arc;
 use std::sync::Mutex;
 
@@ -12,45 +12,41 @@ use objc::runtime::Sel;
 use objc::sel;
 use objc::sel_impl;
 
+use crate::debug_println;
 use crate::utils::attempt_get_lock;
-use crate::utils::catch_and_log_exception;
-use crate::utils::logger;
+use crate::utils::macos::catch_and_log_exception;
+use crate::utils::macos::get_error;
 
 use super::CallbackFn;
 use super::TaskMenuError;
 use super::TaskMenuOperations;
 
-static mut MENU_PTR: Option<Mutex<ObjectId>> = None;
-static mut APP_PTR: Option<Mutex<ObjectId>> = None;
-
 type ObjectId = *mut Object;
-pub type ClickEvent = ObjectId;
 
 #[allow(unexpected_cfgs)]
 extern "C" fn menu_item_clicked(this: &Object, _cmd: Sel, sender: ObjectId) {
-    println!("Test");
-    // unsafe {
-    //     let title: ObjectId = msg_send![sender, title];
-    //     let cstr: *const i8 = msg_send![title, UTF8String];
-    //
-    //     if !title.is_null() && !cstr.is_null() {
-    //         let handlers_ptr = *this.get_ivar::<*mut c_void>("handlers_ptr")
-    //             as *mut Arc<Mutex<HashMap<String, CallbackFn>>>;
-    //         let key = CStr::from_ptr(cstr).to_string_lossy().to_string();
-    //         if !handlers_ptr.is_null() {
-    //             let h_map = handlers_ptr.as_ref();
-    //
-    //             if let Some(handlers) = h_map {
-    //                 attempt_get_lock(handlers, |m| {
-    //                     let handler = m.get(&key);
-    //                     if let Some(cb) = handler {
-    //                         cb()
-    //                     }
-    //                 });
-    //             }
-    //         }
-    //     }
-    // }
+    unsafe {
+        let title: ObjectId = msg_send![sender, title];
+        let cstr: *const i8 = msg_send![title, UTF8String];
+
+        if !title.is_null() && !cstr.is_null() {
+            let handlers_ptr = *this.get_ivar::<ObjectId>("handlers_ptr");
+            if handlers_ptr.is_null() {
+                debug_println!("Handlers pointer is null");
+                return;
+            }
+            let handlers_ptr: &Mutex<HashMap<String, CallbackFn>> =
+                &*(handlers_ptr as *mut Mutex<HashMap<String, CallbackFn>>);
+            let key = CStr::from_ptr(cstr).to_string_lossy().to_string();
+
+            attempt_get_lock(handlers_ptr, |m| {
+                let handler = m.get(&key);
+                if let Some(cb) = handler {
+                    cb(Some(key))
+                }
+            });
+        }
+    }
 }
 
 pub struct TaskMenuBar {
@@ -80,70 +76,66 @@ impl TaskMenuBar {
         handlers: Arc<Mutex<HashMap<String, CallbackFn>>>,
     ) -> Result<ObjectId, TaskMenuError> {
         unsafe {
-            let superclass = class!(NSObject);
-            let cls_name = "AppDelegate";
-            let decl = objc::declare::ClassDecl::new(cls_name, superclass);
+            let result = catch_and_log_exception(
+                |args| {
+                    let superclass = class!(NSObject);
+                    let cls_name = "AppDelegate";
+                    let decl = objc::declare::ClassDecl::new(cls_name, superclass);
 
-            if decl.is_none() {
-                return Err(TaskMenuError::Init(
-                    "Failed to declare AppDelegate class".to_string(),
-                ));
-            }
+                    let mut decl = decl.unwrap();
 
-            let mut decl = decl.unwrap();
+                    decl.add_ivar::<ObjectId>("handlers_ptr");
 
-            decl.add_ivar::<ObjectId>("handlers_ptr");
+                    // Dynamic handler for menu clicks
+                    decl.add_method(
+                        sel!(menu_item_clicked:),
+                        menu_item_clicked as extern "C" fn(&Object, Sel, *mut Object),
+                    );
 
-            // Dynamic handler for menu clicks
-            decl.add_method(
-                sel!(menu_item_clicked:),
-                menu_item_clicked as extern "C" fn(&Object, Sel, *mut Object),
+                    let new_class = decl.register();
+                    let delegate_obj: ObjectId = msg_send![new_class, new]; // Create instance of AppDelegate
+
+                    let handlers_ptr: *mut Mutex<HashMap<String, CallbackFn>> = args as *mut _;
+
+                    (*delegate_obj).set_ivar("handlers_ptr", handlers_ptr as ObjectId);
+                    delegate_obj as *mut std::ffi::c_void
+                },
+                Arc::into_raw(handlers) as *mut _,
             );
 
-            let new_class = decl.register();
-            let delegate_obj: ObjectId = msg_send![new_class, new]; // Create instance of AppDelegate
-
-            if delegate_obj.is_null() {
-                return Err(TaskMenuError::Init(
-                    "Failed to instantiate AppDelegate".to_string(),
-                ));
+            if result.result.is_null() && !result.error.is_null() {
+                let err = get_error(result.error as ObjectId);
+                return Err(TaskMenuError::Init(err));
             }
-            let handlers_ptr: *mut Mutex<HashMap<String, CallbackFn>> =
-                Arc::into_raw(handlers.clone()) as *mut _;
-            (*delegate_obj).set_ivar("handlers_ptr", handlers_ptr as ObjectId);
-            Ok(delegate_obj)
+
+            Ok(result.result as ObjectId)
         }
     }
 }
 
 #[allow(unexpected_cfgs, static_mut_refs)]
 impl TaskMenuOperations for TaskMenuBar {
-    fn set_menu_items(&self, items: Vec<String>) -> Result<(), TaskMenuError> {
-        todo!()
-    }
-
     fn set_quit_button(&self) -> Result<(), TaskMenuError> {
         unsafe {
-            let p = self.app_ref;
-            let menu = self.menu_ref;
-            if p.is_null() || menu.is_null() {
-                return Err(TaskMenuError::Unexpected(
-                    "Failed to set quit btn: Pointer is null".to_string(),
-                ));
-            }
-            let quit_item: ObjectId = msg_send![class!(NSMenuItem), new];
-            if quit_item.is_null() {
-                return Err(TaskMenuError::Unexpected(
-                    "Menu item was not instantiated".to_string(),
-                ));
-            }
-            let quit_title = TaskMenuBar::string_to_nsstring("Quit")?;
+            let result = catch_and_log_exception(
+                |args| {
+                    let menu = args as ObjectId;
+                    let quit_item: ObjectId = msg_send![class!(NSMenuItem), new];
+                    let quit_title = TaskMenuBar::string_to_nsstring("Quit").unwrap();
 
-            let _: () = msg_send![quit_item, setTitle: quit_title];
+                    let _: () = msg_send![quit_item, setTitle: quit_title];
 
-            // Set the quit action
-            let _: () = msg_send![quit_item, setAction: sel!(terminate:)];
-            let _: () = msg_send![menu, addItem: quit_item];
+                    // Set the quit action
+                    let _: () = msg_send![quit_item, setAction: sel!(stop:)];
+                    let _: () = msg_send![menu, addItem: quit_item];
+                    ptr::null_mut::<std::ffi::c_void>()
+                },
+                self.menu_ref as *mut std::ffi::c_void,
+            );
+            if !result.error.is_null() {
+                let err = get_error(result.error as ObjectId);
+                return Err(TaskMenuError::Unexpected(err));
+            }
             Ok(())
         }
     }
@@ -166,11 +158,9 @@ impl TaskMenuOperations for TaskMenuBar {
 
             let _: () = msg_send![menu_item, setTitle: item_title];
 
-            // Store callback in HANDLERS map
-            // HANDLERS.lock().unwrap().insert(title.to_string(), callback);
-
-            // let cb_pointer: fn() = Box::into_raw(Box::new(on_click));
-            // let handlers = self.handlers.insert(btn_title, cb_pointer);
+            attempt_get_lock(&self.handlers, |mut h_m| {
+                h_m.insert(btn_title, on_click);
+            });
 
             // Set target & action
             let app_delegate = self.app_delegate;
@@ -190,74 +180,110 @@ impl TaskMenuOperations for TaskMenuBar {
 
     fn init() -> Result<Self, TaskMenuError> {
         unsafe {
-            catch_and_log_exception(logger, move || {
-                let app: ObjectId = msg_send![class!(NSApplication), sharedApplication];
-                let _: () = msg_send![app, setActivationPolicy: 2]; // Hide Dock icon (Accessory mode)
-                                                                    //
+            let result = catch_and_log_exception(
+                |_| {
+                    let app: ObjectId = msg_send![class!(NSApplication), sharedApplication];
+                    let _: () = msg_send![app, setActivationPolicy: 2]; // Hide Dock icon (Accessory mode)
+                                                                        //
 
-                let system_status_bar: ObjectId = msg_send![class!(NSStatusBar), systemStatusBar];
+                    let system_status_bar: ObjectId =
+                        msg_send![class!(NSStatusBar), systemStatusBar];
 
-                let status_item: ObjectId =
-                    msg_send![system_status_bar, statusItemWithLength: -1.0];
-                let title = TaskMenuBar::string_to_nsstring("🔔").unwrap();
+                    let status_item: ObjectId =
+                        msg_send![system_status_bar, statusItemWithLength: -1.0];
+                    let title = TaskMenuBar::string_to_nsstring("🔔").unwrap();
 
-                let button: ObjectId = msg_send![status_item, button];
-                let _: () = msg_send![button, setTitle: title];
+                    let button: ObjectId = msg_send![status_item, button];
+                    let _: () = msg_send![button, setTitle: title];
 
-                // Create a dynamic menu
-                let menu: ObjectId = msg_send![class!(NSMenu), new];
+                    // Create a dynamic menu
+                    let menu: ObjectId = msg_send![class!(NSMenu), new];
 
-                // Attach the menu to the status item
-                let _: () = msg_send![status_item, setMenu: menu];
+                    // Attach the menu to the status item
+                    let _: () = msg_send![status_item, setMenu: menu];
 
-                let mutex = &mut MENU_PTR;
+                    let boxed: Box<[ObjectId; 2]> = Box::new([app, menu]);
 
-                *mutex = Some(Mutex::new(menu));
-                let app_mutex = &mut APP_PTR;
-                *app_mutex = Some(Mutex::new(app));
-            });
+                    Box::into_raw(boxed) as *mut std::ffi::c_void
+                },
+                ptr::null_mut::<std::ffi::c_void>(),
+            );
+            if result.result.is_null() && !result.error.is_null() {
+                let err = get_error(result.error as ObjectId);
+                return Err(TaskMenuError::Init(err));
+            }
 
-            if APP_PTR.is_none() {
+            let refs_arr = Box::from_raw(result.result as *mut [ObjectId; 2]);
+            if refs_arr.is_empty() {
+                return Err(TaskMenuError::Init("App was not instantiated".to_string()));
+            }
+            let app_ref = refs_arr[0];
+            let menu_ref = refs_arr[1];
+
+            if app_ref.is_null() {
                 return Err(TaskMenuError::Init("App was not instantiated".to_string()));
             }
 
-            let app_ref = APP_PTR.as_ref().unwrap();
-            let app_ref = app_ref
-                .lock()
-                .map_err(|err| TaskMenuError::Init(format!("App lock failed: {:?}", err)))?;
-            let app_ref = app_ref.cast() as ObjectId;
-
-            if MENU_PTR.is_none() {
+            if menu_ref.is_null() {
                 return Err(TaskMenuError::Init("Menu was not instantiated".to_string()));
             }
-            let menu_ref = MENU_PTR.as_ref().unwrap();
-            let menu_ref = menu_ref
-                .lock()
-                .map_err(|err| TaskMenuError::Init(format!("Menu lock failed: {:?}", err)))?;
-            let menu_ref = menu_ref.cast() as ObjectId;
 
-            let handlers_map = Arc::new(Mutex::new(HashMap::new()));
+            let handlers = Arc::new(Mutex::new(HashMap::new()));
+            let app_delegate = TaskMenuBar::create_app_delegate(handlers.clone())?;
 
-            let app_delegate = TaskMenuBar::create_app_delegate(handlers_map.clone())?;
+            if app_delegate.is_null() {
+                return Err(TaskMenuError::Init(
+                    "App delegate was not instantiated".to_string(),
+                ));
+            }
 
             Ok(TaskMenuBar {
                 menu_ref,
                 app_ref,
                 app_delegate,
-                handlers: handlers_map,
+                handlers,
             })
+        }
+    }
+    fn remove_menu_item(&self, btn_title: String) -> Result<(), TaskMenuError> {
+        unsafe {
+            let boxed: Box<(String, ObjectId)> = Box::new((btn_title.clone(), self.menu_ref));
+            let res = catch_and_log_exception(
+                |args| {
+                    let args_union = Box::from_raw(args as *mut (String, ObjectId));
+                    let (title, menu_ref) = *args_union;
+                    let button_item: ObjectId = msg_send![menu_ref, itemWithTitle: title];
+                    let _: () = msg_send![menu_ref, removeItem: button_item];
+                    args
+                },
+                Box::into_raw(boxed) as *mut std::ffi::c_void,
+            );
+            attempt_get_lock(&self.handlers, |mut m| {
+                m.remove(&btn_title);
+            });
+            if !res.error.is_null() {
+                let err = get_error(res.error as ObjectId);
+                Err(TaskMenuError::Init(err))
+            } else {
+                Ok(())
+            }
         }
     }
 
     fn run(&self) -> Result<(), TaskMenuError> {
         unsafe {
-            if !self.app_ref.is_null() {
-                let _: () = msg_send![self.app_ref, run];
-                Ok(())
+            let res = catch_and_log_exception(
+                |app_ref| {
+                    let _: () = msg_send![app_ref as ObjectId, run];
+                    app_ref
+                },
+                self.app_ref as *mut std::ffi::c_void,
+            );
+            if !res.error.is_null() {
+                let err = get_error(res.error as ObjectId);
+                Err(TaskMenuError::Init(err))
             } else {
-                Err(TaskMenuError::Init(
-                    "Failed to run app. Pointer is null".to_string(),
-                ))
+                Ok(())
             }
         }
     }
--- src/app/mod.rs
+++ src/app/mod.rs
@@ -8,13 +8,14 @@ pub enum TaskMenuError {
     Unexpected(String),
 }
 
-type CallbackFn = Box<dyn Fn()>;
+pub type Event = Option<String>;
+pub type CallbackFn = Box<dyn Fn(Event)>;
 
 pub trait TaskMenuOperations: Sized + Sync + Send {
     fn init() -> Result<Self, TaskMenuError>;
-    fn set_menu_items(&self, items: Vec<String>) -> Result<(), TaskMenuError>;
     fn add_menu_item(&self, btn_title: String, on_click: CallbackFn) -> Result<(), TaskMenuError>;
     fn set_quit_button(&self) -> Result<(), TaskMenuError>;
+    fn remove_menu_item(&self, btn_title: String) -> Result<(), TaskMenuError>;
     fn run(&self) -> Result<(), TaskMenuError>;
 }
 
@@ -23,10 +24,10 @@ use macos::TaskMenuBar as PlatformTaskBar;
 
 pub fn init_taskmenu() -> Result<impl TaskMenuOperations, TaskMenuError> {
     let bar = PlatformTaskBar::init()?;
-    bar.set_quit_button()?;
     bar.add_menu_item(
         "About".to_string(),
-        Box::new(move || println!("Clicked on about")),
+        Box::new(move |e| println!("Clicked on {:?}", e)),
     )?;
+    bar.set_quit_button()?;
     Ok(bar)
 }
